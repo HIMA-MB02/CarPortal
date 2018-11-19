611034606023634b12042afa3691f876cfa1567a
HIMA-MB02 <hima.motherboard02@gmail.com>
2018-11-19 13:53:48 +0530

    Update NavController.cs

--- Controllers/NavController.cs
+++ Controllers/NavController.cs
@@ -9,6 +9,7 @@ namespace VahanBlog.Controllers
     public class NavController : Controller
     {
         // GET: Nav
+        //this is the controller for the navigation bar in the Car Portal.
         public PartialViewResult Menu()
         {
             IEnumerable<string> adminCat = new List<string>() { "Blog Posts", "Cars", "Makes" };
