4343b5204578c29f474dc928683345d1d7504119
HIMA-MB02 <hima.motherboard02@gmail.com>
2018-11-18 22:17:15 -0800

    Add files via upload

---
+++ Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using VahanBlog.Infrastructure;
+
+namespace VahanBlog.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthPovider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+            }
+            return result;
+        }
+    }
+}
---
+++ Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using VahanBlog.Concrete;
+using Ninject;
+using System.Linq;
+using Moq;
+using VahanBlog.Abstract;
+using VahanBlog.Entities;
+using VahanBlog.Infrastructure.Concrete;
+using VahanBlog.Infrastructure;
+
+
+namespace VahanBlog.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IBlogPostRepository>().To<EFBlogPostRepository>();
+            kernel.Bind<IAuthPovider>().To<FormsAuthProvider>();
+        }
+    }
+}
