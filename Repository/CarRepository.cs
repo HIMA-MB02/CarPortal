5635b27ef01e59d1e030ed73c8bf785fe13d83cd
HIMA-MB02 <hima.motherboard02@gmail.com>
2018-11-18 22:18:49 -0800

    Add files via upload

---
+++ Repository/CarRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VahanBlog.Concrete;
+using VahanBlog.Models;
+using VahanBlog.Entities;
+
+namespace VahanBlog.Repository
+{
+    public class CarRepository
+    {
+        public EFDbContext context = new EFDbContext();
+        public IEnumerable<Car> Cars { get { return context.Cars; } }
+        public IEnumerable<Images> Images { get { return context.Images; } }
+        public IEnumerable<MakeFilters> MakeFilters { get { return context.MakeFilters; } }
+    }
+}
