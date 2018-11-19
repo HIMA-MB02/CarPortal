53c0656c46eccd71dd8ab2e2f6180be56aa8317e
HIMA-MB02 <hima.motherboard02@gmail.com>
2018-11-18 22:13:05 -0800

    Add files via upload

---
+++ Abstract/IBlogPostRepository.cs
@@ -0,0 +1,26 @@
+﻿using VahanBlog.Entities;
+using System.Collections.Generic;
+
+namespace VahanBlog.Abstract
+{
+    public interface IBlogPostRepository
+    {
+        IEnumerable<BlogPost> BlogPosts { get; }
+
+        void SavePost(BlogPost blogPost);
+
+        BlogPost DeletePost(int postId);
+
+        IEnumerable<Car> Cars { get; }
+
+        void SaveCar(Car car);
+
+        Car DeleteCar(int carId);
+
+        IEnumerable<MakeFilters> MakeFilters { get; }
+
+        void SaveMake(MakeFilters Make);
+
+        MakeFilters DeleteMake(int makeId);
+    }
+}
